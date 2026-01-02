c8e1fc10b6fdc5efe95cad9ef8d43587f094466e
David Rotermund <davrot@uni-bremen.de>
2026-01-02 19:09:21 +0100

    Added feature-linked-url

---
+++ compose/clean_feat_linked_url.sh
@@ -0,0 +1,6 @@
+BRANCH="feature-linked-url"
+rm -rf /workspace/${BRANCH}
+
+cp /workspace/production/compose.yaml_main /workspace/production/compose.yaml
+cp /workspace/production/overleafserver/compose.yaml_main /workspace/production/overleafserver/compose.yaml
+
