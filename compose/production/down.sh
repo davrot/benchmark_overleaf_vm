d6c007d91a84448a2a7c17062b792d0628a834ca
David Rotermund <davrot@uni-bremen.de>
2026-01-04 05:48:29 +0100

    Preparing ldap scripts

--- compose/production/down.sh
+++ compose/production/down.sh
@@ -3,6 +3,8 @@ docker compose down
 echo "Check and stop other stray container"
 docker stop gitbackup
 docker stop oidc
+docker stop saml
+docker stop ldap
 docker stop aiecho
 
 
---
+++ compose/switch_2_feat_authentication_ldap.sh
@@ -0,0 +1,19 @@
+BRANCH="feature-authentication-ldap"
+sudo apt install -y ldap-utils
+sudo rm -rf /workspace/production/ldap/data
+sudo rm -rf /workspace/production/ldap/config
+
+rm -rf /workspace/${BRANCH}
+git clone -b ${BRANCH} https://github.com/davrot/benchmark_overleaf.git /workspace/${BRANCH}
+cd /workspace/${BRANCH}/server-ce
+make build-base
+make build-community
+
+cd /workspace/production/
+sh down.sh
+
+cp /workspace/production/nginx/nginx.conf_main /workspace/production/nginx/nginx.conf
+cp /workspace/production/compose.yaml_${BRANCH} /workspace/production/compose.yaml
+cp /workspace/production/overleafserver/compose.yaml_${BRANCH} /workspace/production/overleafserver/compose.yaml
+
+sh up.sh
